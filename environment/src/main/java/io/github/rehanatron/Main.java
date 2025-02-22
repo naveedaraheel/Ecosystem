c34b83ee1f0336fc02bde2ee3fcee7f5857033dd
RehanAtron <rehanahmed9574@gmail.com>
2025-02-22 21:26:52 +0500

    Test Run

--- environment/src/main/java/io/github/rehanatron/Main.java
+++ environment/src/main/java/io/github/rehanatron/Main.java
@@ -1,7 +1,24 @@
 package io.github.rehanatron;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, JavaFX!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 800, 600);
+
+        primaryStage.setTitle("Ecosystem Simulator");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
 }
